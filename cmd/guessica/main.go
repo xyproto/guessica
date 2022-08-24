a5e3dbc7814a571f8dc5af646aa133f8be188372
Alexander F. Rødseth <rodseth@gmail.com>
2022-08-24 14:21:31 +0200

    New patch release

--- cmd/guessica/main.go
+++ cmd/guessica/main.go
@@ -62,11 +62,14 @@ func main() {
 					o.Printf("<darkred>%s</darkred>\n", err)
 					continue
 				}
+
+				foundNewVersion := !strings.Contains(pkgbuildContents, "pkgver="+ver)
+
 				var sb strings.Builder
 				for _, line := range strings.Split(pkgbuildContents, "\n") {
 					if strings.HasPrefix(line, "pkgver=") {
 						sb.WriteString("pkgver=" + ver + "\n")
-					} else if strings.HasPrefix(line, "pkgrel=") {
+					} else if foundNewVersion && strings.HasPrefix(line, "pkgrel=") {
 						sb.WriteString("pkgrel=1\n")
 					} else if strings.HasPrefix(line, "source=") {
 						sb.WriteString(sourceLine + "\n")
